b401a718a06490cb82eb56b3fc39f62c907d0b7c
caijunqi99 <xiaojianhai.git>
2018-09-26 21:48:52 +0800

    看æµ孩管理

---
+++ application/admin/lang/zh-cn/look.lang.php
@@ -0,0 +1,47 @@
+<?php
+
+//摄像头列表
+$lang['look_camera_title']			    = '摄像头管理';
+$lang['look_camera_name']			    = '摄像头名称';
+$lang['look_camera_sn']			    = 'sn';
+$lang['look_camera_key']			    = 'key';
+$lang['look_camera_area']			    = '所属地区';
+$lang['look_camera_school']			    = '所属学校名称';
+$lang['look_camera_time']			    = '申请安装时间';
+$lang['look_camera_az_area']			    = '绑定班级/区域';
+$lang['look_camera_is_public_area']			    = '是否公共区域';
+$lang['look_camera_import']			    = '导入摄像头安装申请表';
+$lang['look_camera_download']			    = '下载摄像头安装申请表excel模板';
+$lang['look_camera_download_mb']			    = '摄像头安装申请模板';
+$lang['look_camera_wait_an_number']			    = '待安装系统摄像头数量';
+$lang['look_camera_search']			    = '搜索';
+$lang['look_camera_cz_ts']			    = '操作提示';
+$lang['look_camera_file']			    = '选择导入excel文件';
+$lang['look_camera_true']			    = '确认  导入';
+$lang['look_camera_del']			    = '取消';
+$lang['look_camera_sb_bd']			    = '绑定设备信息';
+$lang['look_camera_company']			    = '所属子公司/代理商';
+$lang['look_camera_text']			    = '备注';
+$lang['look_camera_import_ts']			    = '注意：请选择利用摄像头申请表模板填写的excel表进行导入，如果没有严格按模板进行填写导入，会出现数据错误，导致无法使用';
+
+//网络监控
+$lang['look_monitor_title']			    = '网络监控管理';
+
+
+//重温课堂管理
+$lang['look_revisit_title']	= '重温课堂管理';
+$lang['look_revisit_area']			    = '所属地区';
+$lang['look_revisit_school']			    = '所属学校名称';
+$lang['look_revisit_az_area']			    = '绑定班级/区域';
+$lang['look_revisit_black']			    = '回放';
+$lang['look_revisit_jk_ts']			    = '摄像头监控画面,双击画面可全屏观看，全屏时双击返回缩小画面';
+
+
+
+
+
+
+
+
+
+?>
