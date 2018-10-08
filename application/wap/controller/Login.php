c33fb21638e5b528a78fe50820e6a0aced74fcf6
chenzuijianghu <chenzuijianghu@163.com>
2018-10-08 16:22:08 +0800

    密码

--- application/wap/controller/Login.php
+++ application/wap/controller/Login.php
@@ -93,7 +93,7 @@ class Login extends MobileMall
             }
         }else{//登陆
             if ($password && $is_pass == 1){
-                if ($member_info['member_password'] != md5($password)) {//密码对比
+                if ($member_info['member_password'] != md5(trim($password))) {//密码对比
                     output_error('密码填写错误！',array('type'=>input('post.log_type')));
                 }
             }            
