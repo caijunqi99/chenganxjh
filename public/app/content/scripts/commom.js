4dba41e319acc57a9d1e0972d0b1503afca9fabc
caijunqi99 <xiaojianhai.git>
2018-10-10 18:45:22 +0800

    修改

--- public/app/content/scripts/commom.js
+++ public/app/content/scripts/commom.js
@@ -26,15 +26,14 @@ $(function() {
     FastClick.attach(document.body);
 
 
-    //获取cookie中存储的token,member_id
-    var user_token = $.cookie('token');
-    var user_member_id = $.cookie('member_id');
+
 })
 
 //返回函数
 function historyback() {
     if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) { //判断iPhone|iPad|iPod|iOS
-        window.webkit.messageHandlers.backClick();
+        // window.webkit.messageHandlers.backClick();
+        window.webkit.messageHandlers.backClick.postMessage();
     } else if (/(Android)/i.test(navigator.userAgent)) { //判断Android
         Android.backToApp();
     } else { //pc
