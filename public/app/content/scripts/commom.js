1808bf227e67e53bd997086ba9ecc4cb5014acc4
yangyunfeng <yangyunfeng0903@sina.com>
2018-10-10 19:26:13 +0800

    1

--- public/app/content/scripts/commom.js
+++ public/app/content/scripts/commom.js
@@ -34,7 +34,7 @@ function historyback() {
     if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) { //判断iPhone|iPad|iPod|iOS
         // window.webkit.messageHandlers.backClick();
         // window.webkit.messageHandlers.backClick.postMessage();
-        window.history.back(-1);
+        window.webkit.messageHandlers.backClick.postMessage('back!!');
     } else if (/(Android)/i.test(navigator.userAgent)) { //判断Android
         Android.backToApp();
     } else { //pc
