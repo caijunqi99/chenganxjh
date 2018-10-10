dc2ace2d35f9d961259a4910b1dcc3ce75d10172
caijunqi99 <xiaojianhai.git>
2018-10-10 19:45:57 +0800

    修改

--- public/app/content/scripts/API.js
+++ public/app/content/scripts/API.js
@@ -5,3 +5,4 @@ var token = '8f2da8b131304055a274e0b17d33a029';
 //获取cookie中存储的token,member_id
 var user_token = $.cookie('token');
 var user_member_id = $.cookie('member_id');
+alert(user_token);alert(user_member_id);
