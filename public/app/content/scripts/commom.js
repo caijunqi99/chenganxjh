edeaed1cc9177d622188ee80c8b027e34db94cde
caijunqi99 <xiaojianhai.git>
2018-10-10 17:53:48 +0800

    修改

--- public/app/content/scripts/commom.js
+++ public/app/content/scripts/commom.js
@@ -15,14 +15,29 @@ function footerfixed() {
         });
     }
 }
-$(function () {
+$(function() {
     // foot定位底部
     footerfixed();
-    $(window).resize(function () {
+    $(window).resize(function() {
         footerfixed();
     });
 
     //启用fastclick
     FastClick.attach(document.body);
 
+
+    //获取cookie中存储的token,member_id
+    var user_token = $.cookie('token');
+    var user_member_id = $.cookie('member_id');
 })
+
+//返回函数
+function historyback() {
+    if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) { //判断iPhone|iPad|iPod|iOS
+        window.webkit.messageHandlers.backClick();
+    } else if (/(Android)/i.test(navigator.userAgent)) { //判断Android
+        Android.backToApp();
+    } else { //pc
+        window.history.back(-1);
+    };
+}
