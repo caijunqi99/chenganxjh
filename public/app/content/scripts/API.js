46a945e5881880419e3531d954fd3f6189f5d03c
caijunqi99 <xiaojianhai.git>
2018-10-12 15:49:26 +0800

    修改

--- public/app/content/scripts/API.js
+++ public/app/content/scripts/API.js
@@ -1,8 +1,7 @@
 var api = 'http://vip.xiangjianhai.com:8001/index.php/Wap';
 
 //获取cookie中存储的token,member_id
-var user_token = '1c66a0d923de8e6a4490236d35e3099d';
-var user_member_id = 4;
-/*
+// var user_token = '1c66a0d923de8e6a4490236d35e3099d';
+// var user_member_id = 4;
 var user_token = $.cookie('token');
-var user_member_id = $.cookie('member_id');*/
+var user_member_id = $.cookie('member_id');
