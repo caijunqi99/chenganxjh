e34b1b8b80c8cb0dce67b3bcc756bb0f08b2c7fe
caijunqi99 <xiaojianhai.git>
2018-10-17 09:40:49 +0800

    ‘â修å修

--- application/wap/common.php
+++ application/wap/common.php
@@ -33,7 +33,12 @@ function CalculationTime($order_info,$packagetime){
     return $endTime;
 }
 function output_data($datas, $extend_data = array(),$codd=1,$isAssoc = 'false') {
-    
+
+    if(count($datas) == count($datas,1)){
+        $datas = array(
+            0=>$datas
+        );
+    }
     $data = array();
     $data['code'] = isset($datas['error'])?'100':'200';
     if ($codd !=1) $data['code'] = '400';
