ba7f5117b092ea00d4fd3bc0860495d289c351e6
caijunqi99 <xiaojianhai.git>
2018-10-20 15:38:09 +0800

    修改

--- public/wap/js/product_detail.js
+++ public/wap/js/product_detail.js
@@ -174,6 +174,11 @@ $(function() {
                     });
                     $("#add-cart").click(function() {
                         var e = getCookie("key");
+                        if (!e) {
+                            // window.location.href = WapSiteUrl + "/tmpl/member/login.html";
+                            goLogin();
+                            return false
+                        }
                         var t = parseInt($(".buy-num").val());
                         if (!e) {
                             var o = decodeURIComponent(getCookie("goods_cart"));
@@ -301,9 +306,9 @@ $(function() {
                 });
                 $(".kefu").click(function() {
 
-
-                    // window.location.href = WapSiteUrl + "/tmpl/member/chat_info.html?goods_id=" + r + "&t_id=" + e.result.store_info.member_id
                     window.location.href = 'tel：400-000-000';
+                    // window.location.href = WapSiteUrl + "/tmpl/member/chat_info.html?goods_id=" + r + "&t_id=" + e.result.store_info.member_id
+
                 })
             }})
     }
