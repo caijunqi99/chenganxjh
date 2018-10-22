af8924ae403317102b55cdaecd3b349d1bacba50
caijunqi99 <xiaojianhai.git>
2018-10-22 10:52:23 +0800

    修改

--- application/mobile/api/payment/wxpay_h5/wxpay_h5.php
+++ application/mobile/api/payment/wxpay_h5/wxpay_h5.php
@@ -49,10 +49,10 @@ class wxpay_h5
         $sign = $this->sign($data);
         $data['sign'] = $sign;
 
-        halt($data);
+
 
         $result = $this->postXml('https://api.mch.weixin.qq.com/pay/unifiedorder', $data);
-
+        halt($result);
         if ($result['return_code'] != 'SUCCESS') {
             exception($result['return_msg']);
         }
