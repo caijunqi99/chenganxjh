f10491d13de74da31633545a07ea6bd9f494b8f7
caijunqi99 <xiaojianhai.git>
2018-10-25 11:42:32 +0800

    修改

--- public/wap/js/common.js
+++ public/wap/js/common.js
@@ -967,8 +967,7 @@ function fz_token($key,$member_id){
 
 function ago_back(ago=''){
      ago = GetQueryString('ago');
-     alert(ago);
-    if(!ago || e == 'null'){
+    if(!ago || ago == 'null'){
         historyback();
     }else{
         window.history.back(-1);
