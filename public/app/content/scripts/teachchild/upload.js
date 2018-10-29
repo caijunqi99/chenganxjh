daa7dac3e2a7dbd218027eaaea8c0e6a86276be2
lnj1980 <94114960@qq.com>
2018-10-29 11:29:23 +0800

    修改

--- public/app/content/scripts/teachchild/upload.js
+++ public/app/content/scripts/teachchild/upload.js
@@ -1,35 +1,54 @@
 $(function() {
     // 类别
-    $('#category').picker({
-        title: "请选择适用的年级",
-        cols: [
-            {
-              textAlign: 'center',
-              values: ['父母课堂', '宝宝试听', '精品课堂']
-            },
-            {
-              textAlign: 'center',
-              values: ['幼儿园', '小学', '初中', '高中']
-            },
-            {
-              textAlign: 'center',
-              values: ['一年级', '二年级', '三年级', '四年级', '五年级', '六年级']
-            },
-            {
-              textAlign: 'center',
-              values: ['数学', '语文', '英语']
-            }
-        ],
-        onChange: function(p, v, dv) {
-            console.log(p, v, dv);
-        },
+    $('#category').cityPicker({
+        title: "请选择类别",
+        onChange: function(p, v, dv) {},
         onClose: function(p, v, d) {
-            console.log("close");
+            // 判断是否有四级分类
+            var tokens = p.value;
+            if (tokens.length == 3) {
+                $('.isKemu').show();
+                $.ajax({
+                    url: api + '/teacherhome/index',
+                    type: 'POST',
+                    dataType: 'json',
+                    success: function(response) {
+                        // 获取类别
+                        var tabs = response['result'][0]['categorize'];
+                        for (var i = 0; i < tabs.length; i++) {
+                            if (tokens[0] == tabs[i]['gc_id']) {
+                                for (var s = 0; s < tabs[i]['childTwo'].length; s++) {
+                                    if (tokens[1] == tabs[i]['childTwo'][s]['gc_id']) {
+                                        var Two = tabs[i]['childTwo'][s]['childThree']
+                                        for (var x = 0; x < Two.length; x++) {
+                                            if (tokens[2] == Two[x]['gc_id']) {
+                                                var Three = Two[x]['childFour'];
+                                                var params = {
+                                                    textAlign: 'center',
+                                                    values: []
+                                                }
+                                                var data = {};
+                                                for (var r = 0; r < Three.length; r++) {
+                                                    params['values'].push(Three[r]['gc_name']);
+                                                    data[Three[r]['gc_name']] = Three[r]['gc_id'];
+                                                }
+                                                prickKemu(params, data);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                })
+            } else {
+                $('.isKemu').hide();
+            }
         }
     });
 
     // 简介
-    popupClose = function(event){
+    popupClose = function(event) {
         $('#aboutValue').val($('#sizeStatistics').val())
     }
 
@@ -45,14 +64,110 @@ $(function() {
     });
 
     // 选择价格
-    pricePopupClose = function(event){
+    pricePopupClose = function(event) {
         $('.price_boot_wrap p').each(function(index, el) {
-            if($(this).hasClass('action')){
-                $('#price').val($(this).html().split(' ')[1].split('.')[0])
+            if ($(this).hasClass('action')) {
+                $('#price').val($(this).html().split(' ')[1])
             }
         });
         if ($('.price_boot_wrap input').val() != '') {
             $('#price').val($('.price_boot_wrap input').val())
+        }
+    }
+
+    function prickKemu(value, data) {
+        var arr = [];
+        arr.push(value)
+        $('#kemu').picker({
+            title: "请选择科目",
+            cols: arr,
+            onClose: function() {
+                $('#kemu').attr('data-id', data[$('#kemu').val()])
+            }
+        })
+    }
+
+    // 播放视频
+    var Video = document.getElementsByClassName("back")[0];
+    PlayVideo = function(el) {
+        $('.back').get(0).play();
+        $(el).hide();
+    }
+
+    // 监听视频播放完时的操作
+    Video.addEventListener('ended', function() {
+        $('.ply').show();
+    });
+
+    // 暂停视频
+    $('.back').click(function(event) {
+        if (!event.target.paused) {
+            $(this).get(0).pause();
+            $('.ply').show();
+        };
+    });
+
+    // 确认上传和取消
+    isUpload = function(isTrue) {
+        if (isTrue == 0) {
+            if ($('#bang_name').val() == '') {
+                $.alert("请先填写课程标题");
+                return false;
+            } else if ($('#aboutValue').val() == '') {
+                $.alert("请先填写课程简介");
+                return false;
+            } else if ($('#zuozhe').val() == '') {
+                $.alert("请先填写作者的姓名");
+                return false;
+            } else if ($('#category').val() == '') {
+                $.alert("请先选择类别");
+                return false;
+            } else if ($('#price').val() == '') {
+                $.alert("请先填写价格");
+                return false;
+            } else if (!$('#kemu').is(':hidden')) {
+                if ($('#kemu').val() == '') {
+                    $.alert("请先选择科目");
+                    return false;
+                }
+            } else {
+                var params = {
+                    key: user_token,
+                    member_id: user_member_id,
+                    title: $('#bang_name').val(),
+                    profile: $('#aboutValue').val(),
+                    price: $('#price').val(),
+                    author: $('#zuozhe').val(),
+                }
+                var types = $('#category').get(0).dataset.codes;
+                if (types[1] === types[2]) {
+                    params['type'] = types[0];
+                    params['type2'] = types[1];
+                } else {
+                    params['type'] = types[0];
+                    params['type2'] = types[1];
+                    params['type3'] = types[2];
+                }
+                if (!$('kemu').is(':hidden')) {
+                    params['type4'] = $('#kemu').get(0).dataset.id;
+                }
+                // 上传课件
+                // $.ajax({
+                //     url: api + '/teacherupload/index',
+                //     type: 'POST',
+                //     dataType: 'json',
+                //     data: params,
+                //     success: function(response){
+                //         if(response['code'] == 200){
+                //
+                //         }else {
+                //             console.log(response['message']);
+                //         }
+                //     }
+                // })
+            }
+        } else {
+            window.history.back(-1);
         }
     }
 })
