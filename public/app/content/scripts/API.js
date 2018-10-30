1936e486e7dc2dba3eb6ded022a7e0b7dd5bce46
caijunqi99 <xiaojianhai.git>
2018-10-30 20:55:04 +0800

    修i该

--- public/app/content/scripts/API.js
+++ public/app/content/scripts/API.js
@@ -1,6 +1,8 @@
 var api = 'http://vip.xiangjianhai.com:8001/index.php/Wap';
 
 var upload_url = 'http://vip.xiangjianhai.com:8001/uploads';
+
+var http_url = 'http://vip.xiangjianhai.com:8001/';
 //获取cookie中存储的token,member_id
 
 var user_token = $.cookie('token');
