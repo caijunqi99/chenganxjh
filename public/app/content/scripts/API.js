f044069d41d01d89ef230f87de0be79eb5fb6f17
caijunqi99 <xiaojianhai.git>
2018-11-01 11:10:45 +0800

    细小病毒ai

--- public/app/content/scripts/API.js
+++ public/app/content/scripts/API.js
@@ -6,7 +6,7 @@ var http_url = 'http://vip.xiangjianhai.com:8001/';
 
 //获取cookie中存储的token,member_id
 
-// var user_token = $.cookie('token');
-// var user_member_id = $.cookie('member_id');
-var user_token = '336963623fed7ae9d5c7424a9d42311c';
-var user_member_id = 10017;
+var user_token = $.cookie('token');
+var user_member_id = $.cookie('member_id');
+// var user_token = '336963623fed7ae9d5c7424a9d42311c';
+// var user_member_id = 10017;
