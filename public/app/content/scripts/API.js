69c1666f6bf5809ff0f819974945d88a638a2c9b
caijunqi99 <xiaojianhai.git>
2018-11-02 17:13:35 +0800

    xiguai

--- public/app/content/scripts/API.js
+++ public/app/content/scripts/API.js
@@ -9,7 +9,7 @@ var video_url = 'http://pgj4a41j8.bkt.clouddn.com/admin_video_20181101111008_154
 
 //获取cookie中存储的token,member_id
 
-var user_token = $.cookie('token');
-var user_member_id = $.cookie('member_id');
-// var user_token = 'ac9536a3b1c60492e63e985b17e67a2c';
-// var user_member_id = 10017;
+// var user_token = $.cookie('token');
+// var user_member_id = $.cookie('member_id');
+var user_token = 'ac9536a3b1c60492e63e985b17e67a2c';
+var user_member_id = 10017;
