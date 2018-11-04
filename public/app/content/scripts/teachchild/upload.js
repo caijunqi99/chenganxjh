c8d2bd1f2a128bd3848a1233feccd35d611e063d
caijunqi99 <xiaojianhai.git>
2018-11-04 14:25:36 +0800

    秀嘎

--- public/app/content/scripts/teachchild/upload.js
+++ public/app/content/scripts/teachchild/upload.js
@@ -1,6 +1,6 @@
 $(function() {
     fz_video= function(video_url){
-        alert(video_url);
+        alert(video_url);return false;
         video = video_url;
         var _videoSource = document.getElementById("video_true");
         _videoSource.src = video_url;
