b5719aa0b1fcaa357c4bf89d7d6bc734cc3f28af
chenzuijianghu <chenzuijianghu@163.com>
2018-11-05 21:57:58 +0800

    sss

--- public/app/content/scripts/API.js
+++ public/app/content/scripts/API.js
@@ -10,6 +10,6 @@ var http_url = 'http://vip.xiangjianhai.com:8001/';
 
 var user_token = $.cookie('token');
 var user_member_id = $.cookie('member_id');
-// var user_token = 'f92afaaa1d7718314d0c253137a1e129';
+// var user_token = '1227510ce11a55a337e41e1baa4a6f9b';
 // var user_member_id = 10011;
 
