1deb87a2bb51b70afe9a3520cb7c980a5bf019b0
caijunqi99 <xiaojianhai.git>
2018-11-05 22:59:34 +0800

    吸怪

--- public/app/content/scripts/API.js
+++ public/app/content/scripts/API.js
@@ -8,8 +8,8 @@ var http_url = 'http://vip.xiangjianhai.com:8001/';
 //获取cookie中存储的token,member_id
 
 
-var user_token = $.cookie('token');
-var user_member_id = $.cookie('member_id');
-// var user_token = '293fe2f799eda5401957eb59a90209fd';
-// var user_member_id = 10003;
+// var user_token = $.cookie('token');
+// var user_member_id = $.cookie('member_id');
+var user_token = '0f0a1b4d5f51337632433c8770968f68';
+var user_member_id = 10003;
 
