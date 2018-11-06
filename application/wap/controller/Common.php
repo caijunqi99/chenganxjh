f6b9d31e5dcdb8cd14595ba18de5053d8aa5c106
caijunqi99 <xiaojianhai.git>
2018-11-06 16:47:43 +0800

    膝盖i

--- application/wap/controller/Common.php
+++ application/wap/controller/Common.php
@@ -47,10 +47,6 @@ class Common extends MobileMall
      * @time 20181002
      */
     public function address(){
-        $token = trim(input('post.key'));
-        if(empty($token)){
-            output_error('缺少参数token');
-        }
         $where = ' area_parent_id = 0';
         $area = db('area')->field('area_id as code,area_name as name')->where($where)->select();
         if(!empty($area)){
