309f33c364f84700f4be261b8ad9dda4826f3d69
caijunqi99 <xiaojianhai.git>
2018-11-08 15:21:32 +0800

    xiguai

--- public/app/content/scripts/API.js
+++ public/app/content/scripts/API.js
@@ -8,8 +8,8 @@ var http_url = 'http://vip.xiangjianhai.com:8001/';
 //获取cookie中存储的token,member_id
 
 
-// var user_token = $.cookie('token');
-// var user_member_id = $.cookie('member_id');
-var user_token = 'a36f7146a5f91fb4db76c9aa9a78fac1';
-var user_member_id = 10003;
+var user_token = $.cookie('token');
+var user_member_id = $.cookie('member_id');
+// var user_token = '1c339bbcb91294d59a3074e2ff461599';
+// var user_member_id = 10003;
 
