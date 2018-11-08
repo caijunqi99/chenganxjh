6fbcabf4de39adf06071b0e4c1bfa61d5a30b50e
caijunqi99 <xiaojianhai.git>
2018-11-08 14:46:51 +0800

    膝盖i

--- public/app/content/scripts/API.js
+++ public/app/content/scripts/API.js
@@ -8,8 +8,8 @@ var http_url = 'http://vip.xiangjianhai.com:8001/';
 //获取cookie中存储的token,member_id
 
 
-var user_token = $.cookie('token');
-var user_member_id = $.cookie('member_id');
-// var user_token = '65a72aae94563a73e125a952734fd2ea';
-// var user_member_id = 10003;
+// var user_token = $.cookie('token');
+// var user_member_id = $.cookie('member_id');
+var user_token = 'a36f7146a5f91fb4db76c9aa9a78fac1';
+var user_member_id = 10003;
 
