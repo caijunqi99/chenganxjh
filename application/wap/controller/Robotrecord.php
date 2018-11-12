baca1dfca5f2ca9f0cdcb77ee64e4b5ae6ed5946
yangyunfeng <yangyunfeng0903@sina.com>
2018-11-12 11:39:30 +0800

    1

--- application/wap/controller/Robotrecord.php
+++ application/wap/controller/Robotrecord.php
@@ -22,8 +22,13 @@ class Robotrecord extends MobileMall
         if(empty($student_id)){
             output_error('student_id参数有误');
         }
-        $startTime = strtotime(date("Y-m-d",strtotime ( "-1 week"))." 00:00:00");
-        $endTime = time();
+        //前七天日期
+        $days = array ();
+        for($i=0;$i<=7;$i++) {
+            $days[] = date("Y-m-d", strtotime(' -'. $i . 'day'));
+        }
+        $startTime = strtotime($days[7]);
+        $endTime = strtotime($days[0]." 23:59:59");
         $robot_model = Model('Robotreport');
         $where = "student_id=".$student_id." and ioTime>=".$startTime." and ioTime<=".$endTime;
         $SNNumberInfo = $robot_model->getReportList($where);
@@ -33,9 +38,6 @@ class Robotrecord extends MobileMall
             $SNNumberInfo[$k]['ioDate'] = date("Y-m-d",$v['ioTime']);
             $SNNumberInfo[$k]['ioTime'] = date("H:i:s",$v['ioTime']);
             $SNNumberInfo[$k]['ioVideo'] = $path.$v['ioVideo'];
-            if(date("Y-m-d",time()) == date("Y-m-d",$v['ioTime'])){
-                $SNNumberInfo[$k]['ioDate'] = "今天";
-            }
         }
         foreach($SNNumberInfo as $key=>$item){
             if($item['ioFlag']==1){
@@ -46,17 +48,61 @@ class Robotrecord extends MobileMall
                 $result[$item['ioDate']]['leave'] = $item;
                 $result[$item['ioDate']]['leave']['ioFlagName'] = "放学签到";
             }
-            $result[$item['ioDate']]['week'] = $weekarray[date("w",$item['ioTime'])];
+            if(date("Y-m-d",time()) == $item['ioDate']){
+                $result[$item['ioDate']]['week'] = "今天";
+            }else{
+                $result[$item['ioDate']]['week'] = $weekarray[date("w",strtotime($item['ioDate']))];
+            }
+            $result[$item['ioDate']]['ioDate'] = $item['ioDate'];
         }
-        $result = !empty($result)?[$result]:[];
+
+        foreach($days as $dd=>$yy){
+            if(isset($result[$yy])){
+                $result[$yy] == $result[$yy];
+            }else{
+                if(date("Y-m-d",time()) == $yy){
+                    $result[$yy]['week'] = "今天";
+                }else{
+                    $result[$yy]['week'] = $weekarray[date("w",strtotime($yy))];
+                }
+                $result[$yy]['ioDate'] = $yy;
+            }
+        }
+        if(!empty($result)){
+            $result = $this->array_sort($result,'desc');
+            $results[0] = array_values($result);
+        }else{
+            $results = [];
+        }
         $studentInfo = db('student')->alias('s')
             ->join('__SCHOOL__ sc','sc.schoolid=s.s_schoolid','LEFT')
             ->join('__CLASS__ cl','cl.classid=s.s_classid','LEFT')
             ->field("s_id,s_name,sc.name,cl.classname")
             ->where(array("s_id"=>$student_id))->find();
         $name = $studentInfo['name']."-".$studentInfo['classname']."-".$studentInfo['s_name'];
-        $result[1]['name'] = $name;
-        output_data($result);
+        if(!empty($results[0])){
+            $results[1]['name'] = $name;
+        }else{
+            $results['name'] = $name;
+        }
+        output_data($results);
+    }
+
+    function array_sort($arr,$orderby='asc'){
+        $keysvalue = $new_array = array();
+        foreach ($arr as $k=>$v){
+            $keysvalue[$k] = $k;
+        }
+        if($orderby== 'asc'){
+            asort($keysvalue);
+        }else{
+            arsort($keysvalue);
+        }
+        reset($keysvalue);
+        foreach ($keysvalue as $k=>$v){
+            $new_array[] = $arr[$k];
+        }
+        return $new_array;
     }
 
 }
