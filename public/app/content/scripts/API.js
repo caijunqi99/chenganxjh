0f604e53755d49acee1f55522d6941aedac32f48
chenzuijianghu <chenzuijianghu@163.com>
2018-11-15 10:44:41 +0800

    ss

--- public/app/content/scripts/API.js
+++ public/app/content/scripts/API.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var api = 'http://vip.xiangjianhai.com:8001/index.php/Wap';
 
 var upload_url = 'http://vip.xiangjianhai.com:8001/uploads';
@@ -14,20 +13,3 @@ var user_member_id = $.cookie('member_id');
 // var user_token = 'e615ecd8a03941de4bccf369e98ca871';
 // var user_member_id = 100033;
 
-=======
-var api = 'http://vip.xiangjianhai.com:8001/index.php/Wap';
-
-var upload_url = 'http://vip.xiangjianhai.com:8001/uploads';
-
-var http_url = 'http://vip.xiangjianhai.com:8001/';
-
-
-//获取cookie中存储的token,member_id
-
-
-var user_token = $.cookie('token');
-var user_member_id = $.cookie('member_id');
-// var user_token = 'e615ecd8a03941de4bccf369e98ca871';
-// var user_member_id = 100033;
-
->>>>>>> 4a90475c15d03b316038d4f5ca27d1cbd7b5e17f
