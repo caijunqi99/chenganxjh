6f8ba9531bc323cf6ea250f25d1679734588bb86
caijunqi99 <xiaojianhai.git>
2018-11-16 11:02:42 +0800

    修改

--- public/app/content/scripts/API.js
+++ public/app/content/scripts/API.js
@@ -11,6 +11,6 @@ var http_url = 'http://vip.xiangjianhai.com:8001/';
 
 var user_token = $.cookie('token');
 var user_member_id = $.cookie('member_id');
-// var user_token = 'b09baf45b3f11f51081fe86605c82932';
+// var user_token = 'bbe3bff3bf8e584474e1cb2e9d82a67d';
 // var user_member_id = 100033;
 
