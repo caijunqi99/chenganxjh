5b3db1ca2d2f2b0f9bd259d791db692a65d927e4
caijunqi99 <caijunqi99@163.com>
2018-12-22 12:39:24 +0800

    x

--- application/wap/controller/ReClass.php
+++ application/wap/controller/ReClass.php
@@ -18,7 +18,26 @@ class ReClass extends MobileMember
     }
     //重温课堂首页
     public function index(){
-
+        $id = intval(input('post.id'));
+        $begintime=intval(input('post.begintime'));
+        $endtime=intval(input('post.endtime'));
+        $time=strtotime("-4 month");
+        if($begintime<$time){
+            $begintime='';
+        }
+        if($endtime<$time){
+            $endtime='';
+        }
+        $id=$id.",";
+        $vlink = new Vomont();
+        $res= $vlink->SetLogin();
+        $accountid=$res['accountid'];
+        $res=$vlink->Videotape($accountid,$id,$begintime,$endtime);
+        foreach($res['videos'] as $k=> $v){
+            $res['videos'][$k]['begin']=date('Y-m-d H:i',$v['begintime']);
+            $res['videos'][$k]['end']=date('Y-m-d H:i',$v['endtime']);
+        }
+        output_data($res['videos']);
     }
 
     //重温课堂购买片段页
