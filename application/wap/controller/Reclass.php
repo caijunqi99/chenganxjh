823d309e274a15ec4b49abf58535ab3cb96fdea7
caijunqi99 <xiaojianhai.git>
2018-12-25 11:13:41 +0800

    修i改

--- application/wap/controller/Reclass.php
+++ application/wap/controller/Reclass.php
@@ -20,33 +20,39 @@ class Reclass extends MobileMall
         $begintime=intval(input('post.begintime'));
         $endtime=intval(input('post.endtime'));
         $time=strtotime("-4 month");
-
-        //列外
-        if($begintime==''){
-            $begintime=strtotime(date('Y-m-d'.'07:00:00',time()));
-        }
-        if($endtime==''){
-            $endtime=strtotime(date('Y-m-d'.'17:00:00',time()));
-        }
-
+        $member_id=intval(input('post.member_id'));
         if((!empty($begintime) && $begintime<$time) || empty($begintime)){
             $begintime='';
         }
         if((!empty($endtime) && $endtime<$time) || empty($endtime)){
             $endtime='';
         }
-
-
         $id=$id.",";
         $vlink = new Vomont();
         $res= $vlink->SetLogin();
         $accountid=$res['accountid'];
         $res=$vlink->Videotape($accountid,$id,$begintime,$endtime);
-        foreach($res['videos'] as $k=> $v){
-            $res['videos'][$k]['begin']=date('Y-m-d H:i',$v['begintime']);
-            $res['videos'][$k]['end']=date('Y-m-d H:i',$v['endtime']);
+        $video = $res['videos'];
+        if(!empty($video)){
+            //判断是否购买套餐并且套餐没有过期
+            $is_buy_tc= db('packagetime')->where('member_id="'.$member_id.'" AND pkg_type=2')->find();
+            if(!empty($is_buy_tc) && $is_buy_tc['end_time'] <time()){
+                foreach($video as $k=> $v){
+                    $video[$k]['begin']=date('Y-m-d H:i',$v['begintime']);
+                    $video[$k]['end']=date('Y-m-d H:i',$v['endtime']);
+                    $video[$k]['is_buy'] = 1;
+                }
+            }else{
+                foreach($video as $k=> $v){
+                    $video[$k]['begin']=date('Y-m-d H:i',$v['begintime']);
+                    $video[$k]['end']=date('Y-m-d H:i',$v['endtime']);
+                    //判断是否购买片段
+                    $video[$k]['is_buy'] = 2;
+                }
+            }
+
         }
-        output_data($res['videos']);
+        output_data($video);
     }
 
     //重温课堂购买片段页
