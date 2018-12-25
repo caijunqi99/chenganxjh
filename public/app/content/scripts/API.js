7c76f1979367c95e6874655ede20550b5645bf17
caijunqi99 <xiaojianhai.git>
2018-12-25 19:56:20 +0800

    修改

--- public/app/content/scripts/API.js
+++ public/app/content/scripts/API.js
@@ -1,21 +1,21 @@
 
-// var api = 'http://vip.xiangjianhai.com:8001/index.php/Wap';
-// var upload_url = 'http://vip.xiangjianhai.com:8001/uploads';
-// var http_url = 'http://vip.xiangjianhai.com:8001/';
+var api = 'http://vip.xiangjianhai.com:8001/index.php/Wap';
+var upload_url = 'http://vip.xiangjianhai.com:8001/uploads';
+var http_url = 'http://vip.xiangjianhai.com:8001/';
 
 
-var api = 'http://w.xjh.com/index.php/Wap';
-var upload_url = 'http://w.xjh.com/uploads';
-var http_url = 'http://w.xjh.com/';
+// var api = 'http://w.xjh.com/index.php/Wap';
+// var upload_url = 'http://w.xjh.com/uploads';
+// var http_url = 'http://w.xjh.com/';
 
 
 
 //获取cookie中存储的token,member_id
 
 
-// var user_token = $.cookie('token');
-// var user_member_id = $.cookie('member_id');
-var user_token = 'e2290d37a9aa1436029aae14dd1147e9';
-var user_member_id = 10019;
+var user_token = $.cookie('token');
+var user_member_id = $.cookie('member_id');
+// var user_token = 'e2290d37a9aa1436029aae14dd1147e9';
+// var user_member_id = 10019;
 
 
