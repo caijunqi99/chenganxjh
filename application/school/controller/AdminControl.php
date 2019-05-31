5442bc9527f7663e4618c1e2fa4afb9adcf06b66
caijunqi99 <caijunqi99@163.com>
2019-05-31 09:51:36 +0800

    yonghuguanli

--- application/school/controller/AdminControl.php
+++ application/school/controller/AdminControl.php
@@ -150,7 +150,7 @@ class AdminControl extends Controller {
         }
 
         //以下几项不需要验证   langzhiyao修改不需要验证的控制器：Organizes  Schoolinfo 过滤
-        $tmp = array('Index','Schooldesc','Dashboard','Schoolbus','Schoolfood','Coursemanage','Login','Schoolinfo','Classesinfo','Studentinfo','Mlselection','Common');
+        $tmp = array('Index','Schooldesc','Dashboard','Schoolbus','Schoolfood','Coursemanage','Login','Schoolinfo','Classesinfo','Studentinfo','Mlselection','Common','Membercommon');
         if (in_array($act,$tmp)) return true;
         if (in_array($act,$permission) || in_array("$act.$op",$permission)){
             return true;
