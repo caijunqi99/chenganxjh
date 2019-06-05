e6b581a8987f2158ce6a8fa47dde1882f39d19a0
chenzuijianghu <chenzuijianghu@163.com>
2019-06-05 17:47:14 +0800

    sss

--- vendor/topthink/think-captcha/src/Captcha.php
+++ vendor/topthink/think-captcha/src/Captcha.php
@@ -18,7 +18,7 @@ class Captcha
     protected $config = [
         'seKey'    => 'ThinkPHP.CN',
         // 验证码加密密钥
-        'codeSet'  => '2345678abcdefhijkmnpqrstuvwxyzABCDEFGHJKLMNPQRTUVWXY',
+        'codeSet'  => '2345678abcdefhjkmnpqrstuvwxyzABCDEFGHJKLMNPQRTUVWXY',
         // 验证码字符集合
         'expire'   => 1800,
         // 验证码过期时间（s）
@@ -32,7 +32,7 @@ class Captcha
         // 验证码字体大小(px)
         'useCurve' => true,
         // 是否画混淆曲线
-        'useNoise' => true,
+        'useNoise' => false,
         // 是否添加杂点
         'imageH'   => 0,
         // 验证码图片高度
